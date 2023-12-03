1610b6c4f4c01baa45598b994465ffb590eaa5c6
Jordie <jordie.r.belle@gmail.com>
2023-12-03 06:32:09 -0800

    refactored and extended shortcut ps1

--- powershell/create_desktop_index_shortcuts.ps1
+++ powershell/create_desktop_index_shortcuts.ps1
@@ -1,13 +1,15 @@
 <#
 .SYNOPSIS
-Creates desktop shortcuts for "Device Manager", "Add or Remove Programs", and 
-"Edit the system environment variables" within a folder named "Index Shortcuts".
+Creates desktop shortcuts for "Device Manager", "Add or Remove Programs",
+"Edit the system environment variables", "Calculator", and "Default Apps"
+within a folder named "Index Shortcuts". This script enhances the ease of
+finding essential settings and utilities using Windows' PowerToys Search
+program, providing quick access to frequently used tools.
 
 .NOTES
 File Name      : create_desktop_index_shortcuts.ps1
 Author         : Jordie Belle
 Prerequisite   : PowerShell V2
-
 #>
 
 # Function to log updates
@@ -40,7 +42,8 @@ function CreateShortcut {
     if ($targetPath -eq "explorer.exe") {
         $shortcut.WorkingDirectory = "$env:SystemRoot\System32"
     } else {
-        $shortcut.WorkingDirectory = (Get-Item $targetPath).DirectoryName
+        $shortcut.WorkingDirectory = `
+            (Get-Item $targetPath).DirectoryName
     }
 
     $shortcut.Save()
@@ -58,48 +61,86 @@ if (-not (Test-Path $shortcutFolderPath)) {
     Log "Directory $shortcutFolderPath already exists"
 }
 
-# Create a shortcut for "Device Manager" if it doesn't exist
-$deviceManagerPath = "$env:SystemRoot\System32\devmgmt.msc"
-$deviceManagerShortcutPath = Join-Path $shortcutFolderPath "Device Manager.lnk"
-
-if (-not (Test-Path $deviceManagerShortcutPath)) {
-    Log "Creating Device Manager shortcut"
-    CreateShortcut -targetPath $deviceManagerPath -shortcutPath $deviceManagerShortcutPath -name "Device Manager" -description "Shortcut for Device Manager"
-} else {
-    Log "Device Manager shortcut already exists"
-}
-
 # Create a shortcut for "Add or Remove Programs" if it doesn't exist
 $addOrRemoveProgramsPath = "ms-settings:appsfeatures"
-$addOrRemoveProgramsShortcutPath = Join-Path $shortcutFolderPath "Add or Remove Programs.lnk"
+$addOrRemoveProgramsShortcutPath = Join-Path $shortcutFolderPath `
+    "Add or Remove Programs.lnk"
 
 if (-not (Test-Path $addOrRemoveProgramsShortcutPath)) {
     Log "Creating Add or Remove Programs shortcut"
-    CreateShortcut -targetPath "explorer.exe" -shortcutPath $addOrRemoveProgramsShortcutPath -name "Add or Remove Programs" -description "Shortcut for Add or Remove Programs"
+    CreateShortcut -targetPath "explorer.exe" `
+                   -shortcutPath $addOrRemoveProgramsShortcutPath `
+                   -name "Add or Remove Programs" `
+                   -description "Shortcut for Add or Remove Programs"
     
     $WScriptShell = New-Object -ComObject WScript.Shell
-    $shortcut = $WScriptShell.CreateShortcut($addOrRemoveProgramsShortcutPath)
+    $shortcut = $WScriptShell.CreateShortcut(`
+        $addOrRemoveProgramsShortcutPath)
     $shortcut.Arguments = $addOrRemoveProgramsPath
-    $shortcut.IconLocation = "$env:SystemRoot\System32\SystemSettingsAdminFlows.exe,0"
+    $shortcut.IconLocation = "$env:SystemRoot\System32\" `
+        + "SystemSettingsAdminFlows.exe,0"
     $shortcut.Save()
 } else {
     Log "Add or Remove Programs shortcut already exists"
 }
 
-# Create a shortcut for "Edit the system environment variables" if it doesn't exist
+# Create a shortcut for "Edit the system environment variables" if it doesn't
+# exist
 $editEnvVarsPath = "$env:SystemRoot\System32\rundll32.exe"
-$editEnvVarsShortcutPath = Join-Path $shortcutFolderPath "Edit Environment Variables.lnk"
+$editEnvVarsShortcutPath = Join-Path $shortcutFolderPath `
+    "Edit Environment Variables.lnk"
 $editEnvVarsArgs = "sysdm.cpl,EditEnvironmentVariables"
 
 if (-not (Test-Path $editEnvVarsShortcutPath)) {
     Log "Creating Edit Environment Variables shortcut"
-    CreateShortcut -targetPath $editEnvVarsPath -shortcutPath $editEnvVarsShortcutPath -name "Edit Environment Variables" -description "Shortcut for Edit Environment Variables"
+    CreateShortcut -targetPath $editEnvVarsPath `
+                   -shortcutPath $editEnvVarsShortcutPath `
+                   -name "Edit Environment Variables" `
+                   -description "Shortcut for Edit Environment Variables"
     
     $WScriptShell = New-Object -ComObject WScript.Shell
-    $shortcut = $WScriptShell.CreateShortcut($editEnvVarsShortcutPath)
+    $shortcut = $WScriptShell.CreateShortcut(`
+        $editEnvVarsShortcutPath)
     $shortcut.Arguments = $editEnvVarsArgs
-    $shortcut.IconLocation = "$env:SystemRoot\System32\SystemPropertiesAdvanced.exe,0"
+    $shortcut.IconLocation = "$env:SystemRoot\System32\" `
+        + "SystemPropertiesAdvanced.exe,0"
     $shortcut.Save()
 } else {
     Log "Edit Environment Variables shortcut already exists"
+}
+
+# Additional code for "Calculator" shortcut
+$calculatorPath = "$env:SystemRoot\System32\calc.exe"
+$calculatorShortcutPath = Join-Path $shortcutFolderPath "Calculator.lnk"
+
+if (-not (Test-Path $calculatorShortcutPath)) {
+    Log "Creating Calculator shortcut"
+    CreateShortcut -targetPath $calculatorPath `
+                   -shortcutPath $calculatorShortcutPath `
+                   -name "Calculator" `
+                   -description "Shortcut for Calculator"
+} else {
+    Log "Calculator shortcut already exists"
+}
+
+# Additional code for "Default Apps" shortcut
+$defaultAppsPath = "ms-settings:defaultapps"
+$defaultAppsShortcutPath = Join-Path $shortcutFolderPath "Default Apps.lnk"
+
+if (-not (Test-Path $defaultAppsShortcutPath)) {
+    Log "Creating Default Apps shortcut"
+    CreateShortcut -targetPath "explorer.exe" `
+                   -shortcutPath $defaultAppsShortcutPath `
+                   -name "Default Apps" `
+                   -description "Shortcut for Default Apps"
+    
+    $WScriptShell = New-Object -ComObject WScript.Shell
+    $shortcut = $WScriptShell.CreateShortcut(`
+        $defaultAppsShortcutPath)
+    $shortcut.Arguments = $defaultAppsPath
+    $shortcut.IconLocation = "$env:SystemRoot\System32\" `
+        + "SystemSettingsAdminFlows.exe,0"
+    $shortcut.Save()
+} else {
+    Log "Default Apps shortcut already exists"
 }
