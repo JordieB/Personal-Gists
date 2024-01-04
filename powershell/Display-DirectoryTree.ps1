4108f18e5681b764f5ebd6002a03ebccabc82436
Jordie <jordie.r.belle@gmail.com>
2024-01-04 09:35:42 -0800

    add an arg to viz or not viz files

--- powershell/Display-DirectoryTree.ps1
+++ powershell/Display-DirectoryTree.ps1
@@ -1,9 +1,9 @@
 ﻿<#
 .SYNOPSIS
-    Displays a tree of directories up to a specified maximum depth.
+    Displays a tree of directories and optionally files up to a specified maximum depth.
 
 .DESCRIPTION
-    The Get-Tree function lists directories in a tree-like structure up to a specified maximum depth.
+    The Get-Tree function lists directories (and optionally files) in a tree-like structure up to a specified maximum depth.
     It's useful for getting a quick overview of a directory's structure without overwhelming detail.
 
 .PARAMETER rootPath
@@ -12,28 +12,51 @@
 .PARAMETER maxDepth
     The maximum depth of directories to display.
 
+.PARAMETER IncludeFiles
+    A boolean indicating whether to include files in the tree. Defaults to $false.
+
 .EXAMPLE
     Get-Tree -rootPath "C:\" -maxDepth 3
-    Displays the directory tree of the C: drive up to 3 levels deep.
+    Displays the directory tree of the C: drive up to 3 levels deep, without files.
+
+.EXAMPLE
+    Get-Tree -rootPath "C:\" -maxDepth 3 -IncludeFiles $true
+    Displays the directory tree of the C: drive up to 3 levels deep, including files.
 
 .NOTES
     This function is a custom PowerShell implementation and does not use the 'tree' command.
-    It's designed to provide more control over the depth of the directory tree displayed.
+    It's designed to provide more control over the depth and content of the directory tree displayed.
 #>
 function Get-Tree {
     param (
         [string]$rootPath,
-        [int]$maxDepth
+        [int]$maxDepth,
+        [bool]$IncludeFiles = $false
     )
 
     $currentDepth = 1
     function innerGet-Tree {
-        param ([string]$path, [int]$depth)
+        param (
+            [string]$path,
+            [int]$depth
+        )
         if ($depth -le $maxDepth) {
-            Get-ChildItem -Directory -Path $path | ForEach-Object {
-                $dir = $_
-                "$('  ' * ($depth - 1))+- $dir"
-                innerGet-Tree -path $dir.FullName -depth ($depth + 1)
+            $childItems = if ($IncludeFiles) {
+                Get-ChildItem -Path $path
+            } else {
+                Get-ChildItem -Directory -Path $path
+            }
+
+            foreach ($item in $childItems) {
+                $indent = '  ' * ($depth - 1)
+                if ($item.PSIsContainer) {
+                    Write-Host "$indent+- [D] $item"
+                } else {
+                    Write-Host "$indent+- [F] $item"
+                }
+                if ($item.PSIsContainer) {
+                    innerGet-Tree -path $item.FullName -depth ($depth + 1)
+                }
             }
         }
     }
