ea96ff092cd97548bec671d8220416b5d6914c66
Jordie <jordie.r.belle@gmail.com>
2024-08-11 12:42:37 -0700

    Added ability to exclude dirs to Get-Tree PowerShell func

--- powershell/Display-DirectoryTree.ps1
+++ powershell/Display-DirectoryTree.ps1
@@ -4,7 +4,7 @@
 
 .DESCRIPTION
     The Get-Tree function lists directories (and optionally files) in a tree-like structure up to a specified maximum depth.
-    It's useful for getting a quick overview of a directory's structure without overwhelming detail.
+    It allows excluding specified directories from the output for a cleaner view.
 
 .PARAMETER rootPath
     The root path from where the tree will start.
@@ -15,6 +15,9 @@
 .PARAMETER IncludeFiles
     A boolean indicating whether to include files in the tree. Defaults to $false.
 
+.PARAMETER ExcludeDirs
+    An array of directory names to exclude from the tree display.
+
 .EXAMPLE
     Get-Tree -rootPath "C:\" -maxDepth 3
     Displays the directory tree of the C: drive up to 3 levels deep, without files.
@@ -23,18 +26,25 @@
     Get-Tree -rootPath "C:\" -maxDepth 3 -IncludeFiles $true
     Displays the directory tree of the C: drive up to 3 levels deep, including files.
 
+.EXAMPLE
+    Get-Tree -rootPath "C:\" -maxDepth 3 -ExcludeDirs @('.mypy_cache', '.venv', '.git')
+    Displays the directory tree of the C: drive up to 3 levels deep, excluding directories named '.mypy_cache' and '.venv'.
+
 .NOTES
     This function is a custom PowerShell implementation and does not use the 'tree' command.
-    It's designed to provide more control over the depth and content of the directory tree displayed.
+    It's designed to provide more control over the depth, content, and exclusion of the directory tree displayed.
 #>
+
 function Get-Tree {
     param (
-        [string]$rootPath,
-        [int]$maxDepth,
-        [bool]$IncludeFiles = $false
+        [string]$rootPath = ".",
+        [int]$maxDepth = 3,
+        [bool]$IncludeFiles = $false,
+        [string[]]$ExcludeDirs = @()
     )
 
     $currentDepth = 1
+
     function innerGet-Tree {
         param (
             [string]$path,
@@ -42,12 +52,17 @@ function Get-Tree {
         )
         if ($depth -le $maxDepth) {
             $childItems = if ($IncludeFiles) {
-                Get-ChildItem -Path $path
+                Get-ChildItem -Path $path -Force
             } else {
-                Get-ChildItem -Directory -Path $path
+                Get-ChildItem -Directory -Path $path -Force
             }
 
             foreach ($item in $childItems) {
+                # Skip excluded directories
+                if ($item.PSIsContainer -and $ExcludeDirs -contains $item.Name) {
+                    continue
+                }
+
                 $indent = '  ' * ($depth - 1)
                 if ($item.PSIsContainer) {
                     Write-Host "$indent+- [D] $item"
@@ -60,5 +75,6 @@ function Get-Tree {
             }
         }
     }
+
     innerGet-Tree -path $rootPath -depth $currentDepth
 }
