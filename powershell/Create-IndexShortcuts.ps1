ec3c0bb4110afb128e38e3075cacba6c9e878095
Jordie <jordie.r.belle@gmail.com>
2024-09-22 10:27:06 -0700

    new shortcuts, removed global vars, added error handling, improved logging, added PS ver check, updated formatting and modularization

--- powershell/Create-IndexShortcuts.ps1
+++ powershell/Create-IndexShortcuts.ps1
@@ -4,12 +4,23 @@ Creates desktop shortcuts for essential Windows utilities within a folder named 
 
 .DESCRIPTION
 This script enhances ease of access to frequently used tools like "Device Manager", "Add or Remove Programs",
-"Edit the system environment variables", "Calculator", and "Default Apps" using Windows' PowerToys Search.
+"Edit the system environment variables", "Calculator", "Default Apps", and "Game Controllers" using Windows' PowerToys Search.
 
 .NOTES
 Author         : Jordie Belle
-Prerequisite   : PowerShell V5 or higher (for ComObject support)
+Prerequisite   : PowerShell V5 or higher (for COM object support)
 #>
+
+# Check for PowerShell version
+if ($PSVersionTable.PSVersion.Major -lt 5) {
+    Write-Error "This script requires PowerShell version 5 or higher."
+    exit
+}
+
+# Define script-level variables
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+$ShortcutFolderPath = Join-Path $DesktopPath "Index Shortcuts"
+$LogFilePath = Join-Path $ShortcutFolderPath 'IndexShortcuts.log'
 
 # Logging function
 function Write-Log {
@@ -22,7 +33,7 @@ function Write-Log {
     $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
     $logMessage = "[$timestamp] $Message"
     Write-Output $logMessage
-    Add-Content -Path "$global:ShortcutFolderPath\IndexShortcuts.log" -Value $logMessage
+    Add-Content -Path $LogFilePath -Value $logMessage
 }
 
 # Function to create a shortcut
@@ -46,40 +57,77 @@ function New-Shortcut {
         [string]$IconLocation = $null
     )
 
-    $WScriptShell = New-Object -ComObject WScript.Shell
-    $shortcut = $WScriptShell.CreateShortcut($ShortcutPath)
-    $shortcut.TargetPath = $TargetPath
-    $shortcut.Description = $Description
-    $shortcut.WorkingDirectory = if ($TargetPath -eq "explorer.exe") { "$env:SystemRoot\System32" } else { (Get-Item $TargetPath).DirectoryName }
+    try {
+        $WScriptShell = New-Object -ComObject WScript.Shell
+        $shortcut = $WScriptShell.CreateShortcut($ShortcutPath)
+        $shortcut.TargetPath = $TargetPath
+        $shortcut.Description = $Description
+        $shortcut.WorkingDirectory = if ($TargetPath -eq "explorer.exe") { "$env:SystemRoot\System32" } else { (Get-Item $TargetPath).DirectoryName }
 
-    if ($Arguments) { $shortcut.Arguments = $Arguments }
-    if ($IconLocation) { $shortcut.IconLocation = $IconLocation } else { $shortcut.IconLocation = $TargetPath }
+        if ($Arguments) { $shortcut.Arguments = $Arguments }
+        if ($IconLocation) { $shortcut.IconLocation = $IconLocation } else { $shortcut.IconLocation = $TargetPath }
 
-    $shortcut.Save()
-    Write-Log "Created shortcut for $Name."
+        $shortcut.Save()
+        Write-Log "Created shortcut for $Name."
+    }
+    catch {
+        Write-Log "Error creating shortcut for $Name: $_"
+    }
 }
 
 function Create-IndexShortcuts {
-    $global:DesktopPath = [Environment]::GetFolderPath("Desktop")
-    $global:ShortcutFolderPath = Join-Path $global:DesktopPath "Index Shortcuts"
-
     # Ensure the "Index Shortcuts" directory exists
-    if (-not (Test-Path $global:ShortcutFolderPath)) {
-        New-Item -Path $global:ShortcutFolderPath -ItemType Directory | Out-Null
-        Write-Log "Created directory: $global:ShortcutFolderPath"
+    if (-not (Test-Path $ShortcutFolderPath)) {
+        New-Item -Path $ShortcutFolderPath -ItemType Directory | Out-Null
+        Write-Log "Created directory: $ShortcutFolderPath"
     }
 
     # Shortcut creation details
     $shortcuts = @(
-        @{ Name = "Add or Remove Programs"; Path = "explorer.exe"; Description = "Shortcut for Add or Remove Programs"; Args = "ms-settings:appsfeatures"; Icon = "$env:SystemRoot\System32\SystemSettingsAdminFlows.exe,0" },
-        @{ Name = "Edit Environment Variables"; Path = "$env:SystemRoot\System32\rundll32.exe"; Description = "Shortcut for Edit Environment Variables"; Args = "sysdm.cpl,EditEnvironmentVariables"; Icon = "$env:SystemRoot\System32\SystemPropertiesAdvanced.exe,0" },
-        @{ Name = "Calculator"; Path = "$env:SystemRoot\System32\calc.exe"; Description = "Shortcut for Calculator" },
-        @{ Name = "Default Apps"; Path = "explorer.exe"; Description = "Shortcut for Default Apps"; Args = "ms-settings:defaultapps"; Icon = "$env:SystemRoot\System32\SystemSettingsAdminFlows.exe,0" }
+        @{
+            Name = "Add or Remove Programs"
+            Path = "explorer.exe"
+            Description = "Shortcut for Add or Remove Programs"
+            Args = "ms-settings:appsfeatures"
+            Icon = "$env:SystemRoot\System32\SystemSettingsAdminFlows.exe,0"
+        },
+        @{
+            Name = "Edit Environment Variables"
+            Path = "$env:SystemRoot\System32\rundll32.exe"
+            Description = "Shortcut for Edit Environment Variables"
+            Args = "sysdm.cpl,EditEnvironmentVariables"
+            Icon = "$env:SystemRoot\System32\SystemPropertiesAdvanced.exe,0"
+        },
+        @{
+            Name = "Calculator"
+            Path = "$env:SystemRoot\System32\calc.exe"
+            Description = "Shortcut for Calculator"
+        },
+        @{
+            Name = "Default Apps"
+            Path = "explorer.exe"
+            Description = "Shortcut for Default Apps"
+            Args = "ms-settings:defaultapps"
+            Icon = "$env:SystemRoot\System32\SystemSettingsAdminFlows.exe,0"
+        },
+        @{
+            Name = "Game Controllers"
+            Path = "$env:SystemRoot\System32\control.exe"
+            Description = "Shortcut for Game Controllers"
+            Args = "joy.cpl"
+            Icon = "$env:SystemRoot\System32\control.exe,0"
+        },
+        @{
+            Name = "Device Manager"
+            Path = "$env:SystemRoot\System32\devmgmt.msc"
+            Description = "Shortcut for Device Manager"
+            Icon = "$env:SystemRoot\System32\devmgmt.msc,0"
+        }
     )
 
     # Iterate through each shortcut detail and create it
     foreach ($shortcut in $shortcuts) {
-        $shortcutPath = Join-Path $global:ShortcutFolderPath "$($shortcut.Name).lnk"
+        $shortcutPath = Join-Path $ShortcutFolderPath "$($shortcut.Name).lnk"
 
         if (-not (Test-Path $shortcutPath)) {
             New-Shortcut -TargetPath $shortcut.Path -ShortcutPath $shortcutPath -Name $shortcut.Name -Description $shortcut.Description -Arguments $shortcut.Args -IconLocation $shortcut.Icon
@@ -88,3 +136,11 @@ function Create-IndexShortcuts {
         }
     }
 }
+
+# Main function
+function Main {
+    Create-IndexShortcuts
+}
+
+# Execute main function
+Main
